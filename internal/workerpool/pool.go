34771da69b30b4b251989cb5c7ee636efa87a098
HellsKitchen99 <palekseev102@gmail.com>
2026-07-15 22:13:18 +0300

    feat(workerpool): add finished worker pool

--- internal/workerpool/pool.go
+++ internal/workerpool/pool.go
@@ -1,14 +1,25 @@
 package workerpool
 
-import "github.com/HellsKitchen99/camera-processor/internal/domain"
+import (
+	"fmt"
+
+	"github.com/HellsKitchen99/camera-processor/internal/domain"
+	"github.com/sirupsen/logrus"
+)
 
 type WorkerPool struct {
+	yoloDetector YoloDetector
+	annotator    Annotator
+
 	Workers int
 	Jobs    <-chan domain.FrameJob
 }
 
-func NewWorkerPool(workers int, jobs <-chan domain.FrameJob) *WorkerPool {
+func NewWorkerPool(yoloDetector YoloDetector, annoattor Annotator, workers int, jobs <-chan domain.FrameJob) *WorkerPool {
 	return &WorkerPool{
+		yoloDetector: yoloDetector,
+		annotator:    annoattor,
+
 		Workers: workers,
 		Jobs:    jobs,
 	}
@@ -16,10 +27,36 @@ func NewWorkerPool(workers int, jobs <-chan domain.FrameJob) *WorkerPool {
 
 func (w *WorkerPool) StartWorkers() {
 	for i := 0; i < w.Workers; i++ {
-		go w.work()
+		go func(workerId int) {
+			w.worker(workerId)
+		}(i)
 	}
 }
 
-func (w *WorkerPool) work() {
-	// брать кадры из Jobs
+func (w *WorkerPool) worker(workerId int) {
+	for job := range w.Jobs {
+		image := job.Image
+		detections, err := w.yoloDetector.DetectCow(image)
+		if err != nil {
+			logrus.Errorf("CAMERA %v WORKER %v: %v", job.CameraID, workerId, err)
+			continue
+		}
+		if len(detections) == 0 {
+			continue
+		}
+		for _, detection := range detections {
+			fmt.Printf(
+				"worker=%d camera=%d cow confidence=%.2f box=(%.0f %.0f %.0f %.0f)\n",
+				workerId,
+				job.CameraID,
+				detection.Confidence,
+				detection.X1,
+				detection.Y1,
+				detection.X2,
+				detection.Y2,
+			)
+		}
+		finalImage := w.annotator.VisualizeDetections(job.Image, detections)
+		fmt.Println(finalImage)
+	}
 }
